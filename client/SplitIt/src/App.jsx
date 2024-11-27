01e73ab35aa1ecb88c5c68a5f7aae6dd46fa8554
Shriya Seth <123366111+Shriyaseth25@users.noreply.github.com>
2024-11-27 23:21:45 +0530

    Update App.jsx
    Added Dashboard and login pages with routing

--- client/SplitIt/src/App.jsx
+++ client/SplitIt/src/App.jsx
@@ -1,39 +1,16 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginRegister from '../src/Components/Login/LoginRegister.jsx'
+import Dashboard from '../src/Components/Dashboard/Dashboard.jsx'
+import './index.css'
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-
-  useEffect(()=>{
-    console.log("Hello");
-  },[name])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router> 
+      <Routes> 
+        <Route path="/login" element={<LoginRegister />} /> 
+        <Route path="/" element={<Dashboard />} /> 
+        </Routes> 
+        </Router>
   )
 }
 
