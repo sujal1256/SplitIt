6136426a55665e55d5174a964c1f5603f5eb1cef
Shriya Seth <123366111+Shriyaseth25@users.noreply.github.com>
2024-11-27 23:24:31 +0530

    Create Dashboard.jsx
    Dashboard / Home page

---
+++ client/SplitIt/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar.jsx";
+import AddGroups from '../AddGroups/AddGroups.jsx'
+
+function Dashboard() {
+  return (
+    <>
+    <Navbar/>
+    <AddGroups/>
+    </>
+  );
+}
+
+export default Dashboard;
