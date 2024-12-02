d0c76b63a60b0750985560b4c2326052e2d3f1fd
Sujal Malhotra <msujal29012004@gmail.com>
2024-12-02 12:41:47 +0530

    fixed Register function and add toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginRegister from '../src/Components/Login/LoginRegister.jsx'
-import Dashboard from '../src/Components/Dashboard/Dashboard.jsx'
-import './index.css'
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginRegister from "../src/Components/Login/LoginRegister.jsx";
+import Dashboard from "../src/Components/Dashboard/Dashboard.jsx";
+import "./index.css";
+import 'react-toastify/dist/ReactToastify.css';
 
+function App() {
   return (
-    <Router> 
-      <Routes> 
-        <Route path="/login" element={<LoginRegister />} /> 
-        <Route path="/" element={<Dashboard />} /> 
-        </Routes> 
-        </Router>
-  )
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginRegister />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </Router>
+  );
 }
 
-export default App
+export default App;
