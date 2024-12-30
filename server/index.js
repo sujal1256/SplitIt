8512047ab47195c46cd069edec8612b30de4f52c
Sujal Malhotra <msujal29012004@gmail.com>
2024-12-30 23:15:30 +0530

    check root

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express, { json, urlencoded } from 'express'; 
-import dotenv from 'dotenv';
-import path from 'path'; 
-import { userRouter } from './routes/user.route.js'; 
-import { groupRouter } from './routes/group.route.js';
-import { expenseRouter } from './routes/expense.route.js';
-import { connectToMongoDB } from './connect.db.js'; 
-import cookieParser from 'cookie-parser';
-import { storeInvitedUser } from './controllers/group.controller.js'; 
-import { mailRouter } from './routes/mails.route.js';
+import express, { json, urlencoded } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { userRouter } from "./routes/user.route.js";
+import { groupRouter } from "./routes/group.route.js";
+import { expenseRouter } from "./routes/expense.route.js";
+import { connectToMongoDB } from "./connect.db.js";
+import cookieParser from "cookie-parser";
+import { storeInvitedUser } from "./controllers/group.controller.js";
+import { mailRouter } from "./routes/mails.route.js";
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.SERVER_PORT;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Content-type", "application/json");
@@ -27,10 +27,11 @@ app.use(json());
 app.use(cookieParser());
 
 // Serve static files from the 'public' directory
-app.use(express.static('./public')); 
+app.use(express.static("./public"));
 
 connectToMongoDB();
 app.get("/invite", storeInvitedUser);
+app.get("/check", (req, res) => res.send(process.env.BACKEND_URL));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/group", groupRouter);
 app.use("/api/v1/expense", expenseRouter);
