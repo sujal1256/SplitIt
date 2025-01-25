9869f87b8f7655206174781219ef8823e58cbec7
Sujal Malhotra <msujal29012004@gmail.com>
2025-01-25 19:06:53 +0530

    state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,16 +8,15 @@ import About from "./Components/About/About.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
 import Groups from "./Components/UserDashBoard/Groups.jsx";
 import GroupDetails from "./Components/Group/GroupDetails.jsx";
-import { Provider } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { store } from "./redux/store.js";
+
 
 function App() {
-
-  const logged = JSON.parse(localStorage.getItem("logged"));
+   const user = useSelector((store)=>store.user);
+   const logged = user.user;  
 
   return (
-    <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -32,7 +31,6 @@ function App() {
           </Route>
         </Routes>
       </Router>
-    </Provider>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import { Provider, useSelector } from "react-redux";
 
-
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ToastContainer />
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <ToastContainer />
+      <App />
+    </StrictMode>
+  </Provider>
 );
