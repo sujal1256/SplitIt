b089e487a9b60bdab41afc778f0faabf7ee21875
Sujal Malhotra <msujal29012004@gmail.com>
2025-02-28 16:30:54 +0530

    Analytics deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
+import { Analytics } from '@vercel/analytics/vue';
 
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
+      <Analytics />
       <ToastContainer />
       <App />
     </StrictMode>
