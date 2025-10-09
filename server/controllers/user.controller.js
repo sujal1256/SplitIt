adc352a5e846c2f1ea819f96f6da7ed5e0301837
Sujal Malhotra <msujal29012004@gmail.com>
2025-10-10 00:00:58 +0530

    Revert "chore: added cache to make the app more faster"

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,7 +4,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { getRandomOtp } from "../utils/functions.utils.js";
 import { sendOTPEmail } from "../utils/mailer.js";
-import { redisClient } from "../connect.db.js";
 
 async function handleRegister(req, res) {
   try {
@@ -83,7 +82,6 @@ async function handleLogout(req, res) {
   res.clearCookie("accessToken");
   return res.status(200).json(new ApiResponse(200, {}, "Logged out"));
 }
-
 async function handleGetGroups(req, res) {
   const { userId } = req.user;
 
@@ -91,37 +89,13 @@ async function handleGetGroups(req, res) {
     return res.status(400).json(new ApiError(400, "User not found"));
   }
 
-  try {
-    const cachedGroups = await redisClient.get(`groups:${userId}`);
-    if (cachedGroups) {
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(
-            200,
-            JSON.parse(cachedGroups),
-            "Groups fetched successfully from cache"
-          )
-        );
-    }
+  const groups = await Group.find({
+    "members.memberId": userId,
+  });
 
-    const groups = await Group.find({
-      "members.memberId": userId,
-    });
-
-    await redisClient.set(
-      `groups:${userId}`,
-      JSON.stringify(groups),
-      "EX",
-      3600
-    );
-
-    res
-      .status(200)
-      .json(new ApiResponse(200, groups, "Group fetched successfully"));
-  } catch (error) {
-    res.status(500).json(new ApiError(500, "Error fetching groups", error));
-  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, groups, "Group fetched successfully"));
 }
 
 async function handleCheckLoggedIn(req, res) {
